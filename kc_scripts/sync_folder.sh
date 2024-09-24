f9065f9780f424c362f08159c67dbd74eadd5c69
windyd <windyd2@sina.com>
2024-09-24 23:08:29 +0800

    Quartz sync: Sep 24, 2024, 11:08 PM

--- kc_scripts/sync_folder.sh
+++
@@ -1,8 +0,0 @@
-src="/Users/kevin/Documents/obsidianVault/My Life/"
-
-# the parent folder of this shell script
-proj_folder=$(dirname $(dirname $(realpath $0)))
-dst="${proj_folder}/content/My Life/"
-echo "Syncing $src to $dst"
-
-rsync -av --delete "$src" "$dst"
