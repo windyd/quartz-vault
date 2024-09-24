9320851c120af42b2956f94d87a009a8c1724380
windyd <windyd2@sina.com>
2024-09-25 00:07:45 +0800

    Quartz sync: Sep 25, 2024, 12:07 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -67,7 +67,7 @@ const config: QuartzConfig = {
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents(),
+      Plugin.TableOfContents({ showByDefault: true, collapseByDefault: true }),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
